ba248531b3a555788477619bd43def422a3e7218
phillsli-campray <phills.li@campray.com>
2014-11-25 18:21:27 +0800

    Created the middleware project

---
+++ middleware/src/com/uswop/resource/UserInfo.java
@@ -0,0 +1,13 @@
+package com.uswop.resource;
+
+public class UserInfo {
+	private String points;
+
+	public String getPoints() {
+		return points;
+	}
+
+	public void setPoints(String points) {
+		this.points = points;
+	}
+}
